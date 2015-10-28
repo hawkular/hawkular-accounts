2c01c6212dbf60c75b9b355191afe88d9cd43392
Juraci Paixão Kröhling <juraci@kroehling.de>
2015-10-28 15:58:41 +0100

    HAWKULAR-757 - Added base modules and the first tests

---
+++ integration-tests/smoke/src/test/groovy/org/hawkular/accounts/itest/smoke/OrganizationsITest.groovy
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.accounts.itest.smoke
+
+import org.jboss.arquillian.junit.Arquillian
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNotNull
+
+/**
+ * @author Juraci Paixão Kröhling
+ */
+@RunWith(Arquillian.class)
+class OrganizationsITest extends BaseSmokeTest {
+    @Test
+    void createOrganization() {
+        def orgName = 'Acme, Inc'
+        def response = client.post(
+                path: '/hawkular/accounts/organizations',
+                body: [name: orgName]
+        )
+
+        assertEquals(200, response.status)
+        assertNotNull(response.data.id)
+        assertEquals('The company name should have been persisted', orgName, response.data.name)
+    }
+}
---
+++ integration-tests/smoke/src/test/groovy/org/hawkular/accounts/itest/smoke/SecretStoreITest.groovy
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.accounts.itest.smoke
+
+import groovyx.net.http.ContentType
+import groovyx.net.http.RESTClient
+import org.jboss.arquillian.junit.Arquillian
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertTrue
+
+@RunWith(Arquillian.class)
+class SecretStoreITest extends BaseSmokeTest {
+
+    @Test
+    void canGetCurrentPersona() {
+        // just a sanity test before we start for real
+        def response = client.get(path: "/hawkular/accounts/personas/current")
+        assertEquals(200, response.status)
+        assertNotNull(response.data.id)
+        assertTrue(true)
+    }
+
+    @Test
+    void createTokenBasedOnCurrentPersonaCredentials() {
+        def response = client.post(path: "/hawkular/secret-store/v1/tokens/create")
+        assertEquals(200, response.status)
+        assertNotNull(response.data.key)
+        assertNotNull(response.data.secret)
+    }
+
+    @Test
+    void canAuthenticateWithTokenAsJdoe() {
+        def response = client.get(path: "/hawkular/accounts/personas/current")
+        assertEquals(200, response.status)
+        def jdoeId = response.data.id
+
+        response = client.post(path: "/hawkular/secret-store/v1/tokens/create")
+        assertEquals(200, response.status)
+
+        def token = response.data.key
+        def secret = response.data.secret
+        def toEncode = "${token}:${secret}"
+
+        def clientWithTokenAuth = new RESTClient(baseURI, ContentType.JSON)
+        String encodedCredentials = Base64.getEncoder().encodeToString(toEncode.getBytes("utf-8"))
+        clientWithTokenAuth.defaultRequestHeaders.Authorization = "Basic "+ encodedCredentials
+
+        response = clientWithTokenAuth.get(path: "/hawkular/accounts/personas/current")
+        assertEquals("With token/secret should be recognized as jdoe", jdoeId, response.data.id)
+    }
+}
