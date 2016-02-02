6eb4b6d96d7b307e1899855163b04aa8c6703ea3
Juraci Paixão Kröhling <juraci@kroehling.de>
2016-02-02 12:11:19 +0100

    HAWKULAR-998 - Added an optional filter to convert GET parameters to auth data

---
+++ request-parameter-auth-filter/src/main/java/org/hawkular/accounts/requestparameterauthfilter/MsgLogger.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015-2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hawkular.accounts.requestparameterauthfilter;
+
+import org.jboss.logging.Logger;
+import org.jboss.logging.annotations.LogMessage;
+import org.jboss.logging.annotations.Message;
+import org.jboss.logging.annotations.MessageLogger;
+import org.jboss.logging.annotations.ValidIdRange;
+
+/**
+ * @author Juraci Paixão Kröhling
+ */
+@MessageLogger(projectCode = "HAWKACC")
+@ValidIdRange(min = 160000, max = 169999)
+public interface MsgLogger {
+    MsgLogger LOGGER = Logger.getMessageLogger(MsgLogger.class, MsgLogger.class.getPackage().getName());
+
+    @LogMessage(level = Logger.Level.INFO)
+    @Message(id = 160000, value = "Activating request rewrite for auth data in request parameters on deployment [%s].")
+    void activatingRequestRewriter(String deploymentName);
+
+    @LogMessage(level = Logger.Level.TRACE)
+    @Message(id = 160001, value = "No request parameters, skipping.")
+    void skip();
+
+    @LogMessage(level = Logger.Level.TRACE)
+    @Message(id = 160002, value = "Username/password found on request parameters. Converting.")
+    void converting();
+}
